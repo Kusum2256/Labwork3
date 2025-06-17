352fc82f743325c7b39b88fa4e64f018b957a1db
Kusum <pokh.kusum0@gmail.com>
2025-06-17 12:54:17 +0545

    Add files via upload

---
+++ Lab3_q1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Labwork
+{
+    internal class Lab3_q1
+    {
+        delegate void MyDelegate();
+
+        static void ShowMessage1()
+        {
+            Console.WriteLine("Hello from method 1");
+        }
+
+        static void ShowMessage2()
+        {
+            Console.WriteLine("Hello from method 2");
+        }
+
+        static void Main(string[] args)
+        {
+            MyDelegate d = ShowMessage1;
+            d += ShowMessage2;
+            d();
+        }
+    }
+}
+
